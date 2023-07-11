711e7634458204cde099aa733dccfd8d5048340a
Kartik Mehta <77505989+kartikmehta8@users.noreply.github.com>
2023-07-11 12:33:34 +0530

    generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -19,7 +19,7 @@ def generate_content():
                 for file in files:
                     readme.write(f"- [{file}]({os.path.join(directory, file)})\n")
 
-        readme.write('<h3><p align="center">Implemented with ❤️ by <a href="https://www.kartikmehta.xyz">kartikmehta8</a></p></h3>\n\')
+        readme.write('<h3><p align="center">Implemented with ❤️ by <a href="https://www.kartikmehta.xyz">kartikmehta8</a></p></h3>\n\n')
         readme.write('<h6><p align="center">Automated by GitHub Actions</p></h6>')
 
 if __name__ == '__main__':
