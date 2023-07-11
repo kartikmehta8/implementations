92fe25a3b4775c8aef0a2b676e9527f855d1b149
Kartik Mehta <77505989+kartikmehta8@users.noreply.github.com>
2023-07-11 12:49:51 +0530

    generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -1,4 +1,5 @@
 import os
+import string
 
 def generate_content():
     with open('README.md', 'w') as readme:
@@ -17,7 +18,12 @@ def generate_content():
                 readme.write(f"### {dire}\n")
             
                 for file in files:
-                    readme.write(f"- [{file}]({os.path.join(directory, file)})\n")
+                    fi = str(file)
+                    fi = fi[:-4]
+                    fi = fi.split("_")
+                    fi = " ".join(fi)
+                    fi = string.capwords(fi, sep=None)
+                    readme.write(f"- [{fi}]({os.path.join(directory, file)})\n")
 
         readme.write('<h3><p align="center">Implemented with ❤️ by <a href="https://www.kartikmehta.xyz">kartikmehta8</a></p></h3>\n\n')
         readme.write('<h6><p align="center">Automated by GitHub Actions</p></h6>')
