2d78f26627263735e9543701d067e4e348ccf31f
Kartik Mehta <77505989+kartikmehta8@users.noreply.github.com>
2023-07-14 17:18:19 +0530

    generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -4,6 +4,7 @@ import string
 def generate_content():
     with open('README.md', 'w') as readme:
         readme.write('## Implementations\n\n')
+        num_of_files = 0
         
         for root, dirs, files in os.walk('.'):
             if root == '.':
@@ -17,6 +18,7 @@ def generate_content():
                 readme.write(f"### {dire}\n")
             
                 for file in files:
+                    num_of_files = num_of_files+1
                     fi = str(file)
                     fi = fi[:-4]
                     fi = fi.split("_")
@@ -24,6 +26,7 @@ def generate_content():
                     fi = string.capwords(fi, sep=None)
                     readme.write(f"- [{fi}]({os.path.join(directory, file)})\n")
 
+        readme.write(f'##Total Problems Solved: {num_of_files}\n\n')
         readme.write('<h3><p align="center">Implemented with ❤️ by <a href="https://www.kartikmehta.xyz">kartikmehta8</a></p></h3>\n\n')
         readme.write('<h6><p align="center">Automated by GitHub Actions</p></h6>')
 
